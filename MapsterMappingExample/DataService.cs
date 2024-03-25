c597236c4a1ac66eea1b83a8419fd1b090f471d8
eshohag <eshohag@outlook.com>
2024-03-25 20:54:41 +0600

    Add project files.

---
+++ MapsterMappingExample/DataService.cs
@@ -0,0 +1,35 @@
+﻿using MapsterMappingExample.Models;
+
+namespace MapsterMappingExample
+{
+    public class DataService
+    {
+        public static List<GameCharacter> GetData()
+        {
+            var gameCharacters = new List<GameCharacter>
+            {
+                new GameCharacter
+                {
+                    Id = 1,
+                    Name = "Ellie",
+                    Location = new Location
+                    {
+                        Name = "Jackson",
+                        Description = "A well-protected settlement in Wyoming."
+                    }
+                },
+                new GameCharacter
+                {
+                    Id = 2,
+                    Name = "Joel",
+                    Location = new Location
+                    {
+                        Name = "Boston",
+                        Description = "A city in ruins with dangerous factions."
+                    }
+                }
+            };
+            return gameCharacters;
+        }
+    }
+}
---
+++ MapsterMappingExample/MapperConfig.cs
@@ -0,0 +1,17 @@
+﻿using Mapster;
+using MapsterMappingExample.Models;
+
+namespace MapsterMappingExample
+{
+    public class MapperConfig
+    {
+        public static void ConfigMapster()
+        {
+            TypeAdapterConfig<GameCharacter, GameCharacterDto>.NewConfig()
+                .Map(dest => dest.FullName, src => src.Name);
+
+            TypeAdapterConfig<Location, LocationDto>.NewConfig()
+                .Map(dest => dest.Name, src => src.Name.ToUpper());
+        }
+    }
+}
